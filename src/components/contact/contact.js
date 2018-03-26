df8b07bd325d9def34dacd2ec0bbe108757008e5
cherezzabo <cherezzabo@gmail.com>
2018-03-26 21:09:32 +0300

    hotfix: remove doubled photos, fix Yandex map

--- src/components/contact/contact.js
+++ src/components/contact/contact.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import template from './contact.html';
+import { yandexMap, ymapMarker } from 'vue-yandex-maps';
 
 export default Vue.extend({
   template,
+  components: { yandexMap, ymapMarker }
 });
